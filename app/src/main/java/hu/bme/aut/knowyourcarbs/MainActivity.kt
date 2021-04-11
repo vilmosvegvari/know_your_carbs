2fc13d1fb02c27c31a9c76f94c3dc5d8cd605bc0
Végvári Vilmos <vegvari.vili@gmail.com>
2021-04-11 12:49:03 +0200

    Adding Recipes Activity with an empty ViewModel

--- app/src/main/java/hu/bme/aut/knowyourcarbs/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/MainActivity.kt
@@ -1,11 +1,26 @@
 package hu.bme.aut.knowyourcarbs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import hu.bme.aut.knowyourcarbs.recipes.RecipesActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation(){
+        val recipeButton = findViewById<Button>(R.id.recipesButton)
+        val favButton = findViewById<Button>(R.id.favouritesButton)
+        val aboutButton = findViewById<Button>(R.id.aboutButton)
+
+        recipeButton.setOnClickListener{
+            val intent = Intent(this, RecipesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/recipes/RecipesViewModel.kt
@@ -0,0 +1,7 @@
+package hu.bme.aut.knowyourcarbs.recipes
+
+import androidx.lifecycle.ViewModel
+
+class RecipesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
