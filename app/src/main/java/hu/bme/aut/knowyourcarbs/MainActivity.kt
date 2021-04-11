14e3bce696be479c7cc23884cabe1c4bcb7531b6
Végvári Vilmos <vegvari.vili@gmail.com>
2021-04-11 13:52:59 +0200

    Adding Hilt as a dependency

--- app/src/main/java/hu/bme/aut/knowyourcarbs/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/MainActivity.kt
@@ -4,8 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import hu.bme.aut.knowyourcarbs.recipes.RecipesActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
