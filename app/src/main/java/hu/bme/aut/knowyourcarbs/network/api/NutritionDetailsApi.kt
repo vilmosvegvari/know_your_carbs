09a551ed2c733ba93c2057ec8f685980b362d5b0
Végvári Vilmos <vegvari.vili@gmail.com>
2021-04-27 19:56:07 +0200

    Adding Api layer

---
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/network/api/NutritionDetailsApi.kt
@@ -0,0 +1,29 @@
+package hu.bme.aut.jokes.data.network.api
+
+import retrofit2.http.*
+import retrofit2.Response
+import okhttp3.RequestBody
+
+import hu.bme.aut.jokes.data.network.model.Recipe
+import hu.bme.aut.jokes.data.network.model.RecipeResponse
+
+interface NutritionDetailsApi {
+    /**
+     * Get nutrition info
+     * 
+     * Responses:
+     *  - 200: Recipe object containing number of servings (yield), total calories for the recipe (calories), nutrient content by nutrient type (totalNutrients, totalDaily), diet and health classification (dietLabels, healthLabels)
+     *  - 404: The specified URL was not found or couldn’t be retrieved
+     *  - 422: Couldn’t parse the recipe or extract the nutritional info
+     *  - 555: Recipe with insufficient quality to process correctly
+     * 
+     * @param appId Application Id that you request from the service. Your 3scale application ID. 
+     * @param appKey Application Key that you request from the service. Your 3scale application key. 
+     * @param body  
+     * @param force Forces the re-evaluation of the recipe. The value, if any, is ignored. (optional)
+    * @return [RecipeResponse]
+     */
+    @POST("nutrition-details")
+    suspend fun handleNutritionInfoPost(@Query("app_id") appId: kotlin.String, @Query("app_key") appKey: kotlin.String, @Body body: Recipe, @Query("force") force: kotlin.String? = null): Response<RecipeResponse>
+
+}
