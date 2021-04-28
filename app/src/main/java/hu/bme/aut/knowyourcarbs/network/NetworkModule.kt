2f540ac79a99cc9f2759930de3e6e7d6efa3f583
Végvári Vilmos <vegvari.vili@gmail.com>
2021-04-28 11:29:03 +0200

    Added network module

---
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/network/NetworkModule.kt
@@ -0,0 +1,49 @@
+package hu.bme.aut.knowyourcarbs.network
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import hu.bme.aut.knowyourcarbs.network.api.NutritionDetailsApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+abstract class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "https://api.edamam.com/api"
+
+        @Provides
+        @Singleton
+        fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                })
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideRealJokesApi(retrofit: Retrofit): NutritionDetailsApi  = retrofit.create()
+
+    }
+}
