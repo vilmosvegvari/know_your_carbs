ca0839fc5c7edf46545519107baf757f429c2414
Végvári Vilmos <vegvari.vili@gmail.com>
2021-04-28 12:00:46 +0200

    Added Dbmodule to hilt

---
+++ app/src/main/java/hu/bme/aut/knowyourcarbs/data/DbModule.kt
@@ -0,0 +1,30 @@
+package hu.bme.aut.knowyourcarbs.data
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import hu.bme.aut.knowyourcarbs.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class DbModule {
+    companion object {
+        private const val DB_NAME = "carbs.db"
+
+        @Provides
+        @Singleton
+        fun provideJokesDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideJokeDao(appDatabase: AppDatabase) = appDatabase.recipeDao()
+    }
+}
